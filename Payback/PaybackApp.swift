f66ad1ec34bd558dc9ab79c7fa1a7898b624254c
Emre Boyacı <a.boyaci@enbw.com>
2022-12-03 21:12:19 +0300

    Initial Commit

---
+++ Payback/PaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaybackApp.swift
+//  Payback
+//
+//  Created by Emre Boyaci on 3.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct PaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
